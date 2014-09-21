f3593f8492874357d3a0ab25bb163b5df7313ec4
sizur <eugene.grigoriev@gmail.com>
2014-09-21 15:02:44 -0700

    graphviz imagemagick notugly, ledger, ofxclient

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 set -gx TERM xterm-256color
-set -gx PATH /home/sizur/racket/bin /home/sizur/.opam/4.01.0/bin /usr/lib64/mpi/gcc/openmpi/bin /home/sizur/bin /usr/local/bin /usr/bin /bin /usr/bin/X11 /usr/X11R6/bin /usr/games
+set -gx PATH /home/sizur/ledger /home/sizur/.gem/ruby/2.0.0/bin /home/sizur/racket/bin /home/sizur/.opam/4.01.0/bin /usr/lib64/mpi/gcc/openmpi/bin /home/sizur/bin /usr/local/bin /usr/bin /bin /usr/bin/X11 /usr/X11R6/bin /usr/games
 set -gx MANPATH /home/sizur/racket/share/man /home/sizur/.opam/4.01.0/man /usr/lib64/mpi/gcc/openmpi/share/man /usr/local/man /usr/share/man
 set -gx CAML_LD_LIBRARY_PATH /home/sizur/.opam/4.01.0/lib/stublibs
 set -gx PERL5LIB /home/sizur/.opam/4.01.0/lib/perl5 $PERL5LIB
