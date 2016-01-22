e81e09bf737f5ad8230d52db0bc1acd019afd534
Eugene Grigoriev <egrigoriev@linkedin.com>
2016-01-21 18:02:29 -0800

    various python integrations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 
   (setq package-archives
         '(("org"          . "http://orgmode.org/elpa/")
+          ("elpy"         . "https://jorgenschaefer.github.io/packages/")
           ("gnu"          . "https://elpa.gnu.org/packages/")
           ("melpa"        . "https://melpa.org/packages/")
           ("melpa-stable" . "https://stable.melpa.org/packages/")
